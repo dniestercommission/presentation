849ff3f260a3917a49972d828d16d81ed8f6cde4
Alex <alexandr.volkoff@gmail.com>
2025-11-24 14:50:19 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Background Switcher
+
+    const hero = document.getElementById('hero');
+    const defaultBg = "images/default.jpg";
+
+    function changeHero(img) {
+        hero.style.backgroundImage = `url('${img}')`;
+    }
+
+    function resetHero() {
+        hero.style.backgroundImage = `url('${defaultBg}')`;
+    }
+
+
+
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+    hero.style.backgroundPositionY = (offset * 0.3) + "px";
+});
+
+
+
+// Smooth scrolling to next section
+document.querySelector(".scroll-down a").addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector("#news-section").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+
+
+document.querySelector(".scroll-down a").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector("#news-section");
+    const offsetTop = target.offsetTop;
+
+    window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+});
